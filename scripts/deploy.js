c526632c25a9681d6ccd3ecd8ffabea08961a663
Zataxo <hassan.og@outlook.com>
2023-08-20 08:53:18 -0700

    deploying to test net

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,9 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+ const token = await hre.ethers.deployContract("kyorakuAshesi");
+ token.waitForDeployment();
+ // then run in terminal :  npx hardhat run --network sepolia scripts/deploy.js
 }
 
 // We recommend this pattern to be able to use async/await everywhere
